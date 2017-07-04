110ef2bedc638d0d51bb07320d266c58721cac95
Chih-Cheng Liang <chihchengliang@gmail.com>
2017-07-04 15:39:20 +0800

    minimalism api done

--- api.py
+++ api.py
@@ -1,16 +1,29 @@
+import json
 import os
-from flask import Flask, request, redirect, url_for, flash, send_from_directory
+
+import cv2
+from darkflow.net.build import TFNet
+from flask import (Flask, flash, jsonify, redirect, request,
+                   send_from_directory, url_for)
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
+
+options = {"model": "cfg/yolo.cfg",
+           "load": "bin/yolo.weights",
+           "threshold": 0.3}
+tfnet = TFNet(options)
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -27,9 +40,13 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(save_path)
+            img = cv2.imread(save_path)
+            predictions = tfnet.return_predict(img)
+            for box in predictions:
+                box["confidence"] = float(box["confidence"])
+            return jsonify(predictions)
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -40,10 +57,12 @@ def upload_file():
     </form>
     '''
 
+
 @app.route('/uploads/<path:filename>')
 def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename, as_attachment=True)
+
 
 @app.route('/uploaded_file', methods=['GET'])
 def uploaded_file():
